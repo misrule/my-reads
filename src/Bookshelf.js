e59028ab154a54ace90a9d91e76bc569fb885415
misrule <sy.smythe@gmail.com>
2017-10-07 12:40:05 -0700

    Implment shelf-changing functionality
    Add BookCase configuration variable (JSON) to App
    Various bug fixes.

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -5,20 +5,28 @@ class BookShelf extends React.Component {
 
   render() {
 
-    const { books } = this.props
-    
+    const { books, onChangeShelf } = this.props
+
     return(
 
       <div className="bookshelf">
         <h2 className="bookshelf-title">{this.props.shelfName}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            
-              {books.map((book) => (
-                <li key={book.id}>
-                  <Book book={book} />
-                </li>
-                ))}
+
+              {
+                books.length > 0 && (
+                  books.map( book => (
+                    <li key={book.id}>
+                      <Book book={book} onChangeShelf={onChangeShelf} />
+                    </li>
+                )))
+              }
+              {
+                books.length <= 0 && (
+                  <div><h4 style={{ color: '#808B96' }}>No books on this bookshelf yet.</h4></div>
+                )
+              }
           </ol>
         </div>
       </div>
--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -1,17 +1,22 @@
 import React from 'react'
 
 class ShelfChanger extends React.Component {
-  render() {
+
+  render() {    
+    const { book, onChangeShelf } = this.props
+
     return (
       <div className="book-shelf-changer">
-        <select>
+        <select 
+            defaultValue={book.shelf} 
+            onChange={(event) => onChangeShelf(book, event.target.value)}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
           <option value="none">None</option>
         </select>
-      </div>      
+      </div>
     );
   }
 }
